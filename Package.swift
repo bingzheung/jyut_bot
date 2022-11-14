41d9efb2d97f3235538df6a26af0ef10c4528ac3
Bing Zheung <bing@ososo.io>
2022-11-14 10:56:12 +0800

    Update

--- Package.swift
+++ Package.swift
@@ -3,28 +3,40 @@
 
 import PackageDescription
 
+let packageDependencies: [Package.Dependency] = {
+        #if os(Linux)
+        return [.package(url: "https://github.com/ososoio/SQLite3.git", from: "1.0.0"),
+                .package(url: "https://github.com/rapierorg/telegram-bot-swift.git", from: "2.1.2")]
+        #else
+        return [.package(url: "https://github.com/rapierorg/telegram-bot-swift.git", from: "2.1.2")]
+        #endif
+}()
+
+let targetDependencies: [Target.Dependency] = {
+        #if os(Linux)
+        return [.product(name: "SQLite3", package: "SQLite3"),
+                .product(name: "TelegramBotSDK", package: "telegram-bot-swift")]
+        #else
+        return [.product(name: "TelegramBotSDK", package: "telegram-bot-swift")]
+        #endif
+}()
+
 let package = Package(
-    name: "JyutBot",
-    products: [
+        name: "JyutBot",
+        products: [
                 .executable(name: "jyutbot", targets: ["JyutBot"])
-    ],
-    dependencies: [
-                .package(url: "https://github.com/rapierorg/telegram-bot-swift.git", from: "2.1.2"),
-                .package(url: "https://github.com/apple/swift-log.git", from: "1.4.4")
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .executableTarget(
-            name: "JyutBot",
-            dependencies: [
-                    .product(name: "TelegramBotSDK", package: "telegram-bot-swift"),
-                    .product(name: "Logging", package: "swift-log")
-            ]
-        ),
-        .testTarget(
-            name: "JyutBotTests",
-            dependencies: ["JyutBot"]
-        ),
-    ]
+        ],
+        dependencies: packageDependencies,
+        targets: [
+                // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+                // Targets can depend on other targets in this package, and on products in packages this package depends on.
+                .executableTarget(
+                        name: "JyutBot",
+                        dependencies: targetDependencies
+                ),
+                .testTarget(
+                        name: "JyutBotTests",
+                        dependencies: ["JyutBot"]
+                ),
+        ]
 )
--- Sources/JyutBot/JyutBot.swift
+++ Sources/JyutBot/JyutBot.swift
@@ -3,18 +3,27 @@ import TelegramBotSDK
 
 @main
 public struct JyutBot {
+
+        private static func placeholderLogger(_ text: String) { }
+
         public static func main() {
                 let token = readToken(from: "TELEGRAM_JYUT_BOT_TOKEN")
                 let bot = TelegramBot(token: token)
+                bot.logger = placeholderLogger
 
                 let controller = BotController(bot: bot)
 
                 let router = Router(bot: bot)
+                router["test"] = controller.test
                 router["help"] = controller.help
                 router["start"] = controller.help
                 router["app"] = controller.app
                 router["ios"] = controller.app
-                router.unmatched = controller.help
+                router["ping", [.slashRequired]] = controller.ping
+                router.partialMatch = { context -> Bool in
+                        return true
+                }
+                router.unmatched = controller.fallbackHandler
 
                 while let update = bot.nextUpdateSync() {
                         _ = try? router.process(update: update)
@@ -30,6 +39,11 @@ struct BotController {
                 self.bot = bot
         }
 
+        func test(context: Context) -> Bool {
+                context.respondPrivatelyAsync("absolutely", groupText: "absolutely")
+                return true
+        }
+
         func help(context: Context) -> Bool {
                 guard let from = context.message?.from else { return false }
 
@@ -43,12 +57,6 @@ struct BotController {
 
                 撳 /app 獲取
                 粵拼輸入法 App Store 連結。
-
-                發 「/add +要加嘅詞條」，
-                可向我哋建議添加粵拼詞條。
-
-                發 「/feedback +你嘅反饋」，
-                向 粵拼bot 提出反饋同建議
                 """
 
                 context.respondPrivatelyAsync(responseText, groupText: responseText)
@@ -64,6 +72,152 @@ struct BotController {
 
                 context.respondPrivatelyAsync(responseText, groupText: responseText)
                 return true
+        }
+
+        func ping(context: Context) -> Bool {
+                guard let messageText: String = context.message?.text else { return false }
+                let filteredText: String = filteredCJKV(text: messageText)
+                guard !filteredText.isEmpty else {
+                        let tipText: String = "/ping +粵語字詞"
+                        context.respondPrivatelyAsync(tipText, groupText: tipText)
+                        return true
+                }
+                let responseText: String = {
+                        let text: String = messageText.replacingOccurrences(of: "/ping", with: "", options: .anchored).trimmingCharacters(in: .whitespacesAndNewlines).trimmingCharacters(in: .controlCharacters)
+                        let matched = lookup(text: text)
+                        if matched.romanizations.isEmpty {
+                                let question: String = Array(repeating: "?", count: text.count).joined(separator: " ")
+                                return text + "：\n" + question
+                        } else {
+                                let romanization: String = matched.romanizations.joined(separator: "\n")
+                                return matched.text + "：\n" + romanization
+                        }
+                }()
+                context.respondPrivatelyAsync(responseText, groupText: responseText)
+                return true
+        }
+
+        func fallbackHandler(context: Context) -> Bool {
+
+                // aka. (context.chatId ?? 0) > 0
+                let isPrivateChat = context.message?.chat.type == .privateChat
+                guard isPrivateChat else {
+                        // logger.notice("Incomprehensible message from group chat.")
+                        return true
+                }
+
+                let text: String = context.message?.text ?? ""
+                guard !text.isEmpty else { return true }
+                guard text != "?" && text != "？" else {
+                        _ = help(context: context)
+                        return true
+                }
+
+                let filteredText: String = filteredCJKV(text: text)
+                guard !filteredText.isEmpty else {
+                        // logger.notice("Incomprehensible message.")
+                        context.respondPrivatelyAsync("我聽毋明 😥", groupText: "我聽毋明 😥")
+                        // logger.info("Sent fallback() response back.")
+                        return true
+                }
+
+                let responseText: String = {
+                        let text: String = text.trimmingCharacters(in: .whitespacesAndNewlines).trimmingCharacters(in: .controlCharacters)
+                        let matched = lookup(text: text)
+                        if matched.romanizations.isEmpty {
+                                let question: String = Array(repeating: "?", count: text.count).joined(separator: " ")
+                                return text + "：\n" + question
+                        } else {
+                                let romanization: String = matched.romanizations.joined(separator: "\n")
+                                return matched.text + "：\n" + romanization
+                        }
+                }()
+
+                context.respondPrivatelyAsync(responseText, groupText: responseText)
+                return true
+        }
+
+
+        // MARK: - Search Jyutpings
+
+        private func filteredCJKV(text: String) -> String {
+                return text.unicodeScalars.filter({ $0.properties.isIdeographic }).map({ String($0) }).joined()
+        }
+        private func ideographicBlocks(text: String) -> [(text: String, isIdeographic: Bool)] {
+                var blocks: [(String, Bool)] = []
+                var ideographicCache: String = ""
+                var otherCache: String = ""
+                var lastWasIdeographic: Bool = true
+                for character in text {
+                        let isIdeographic: Bool = character.unicodeScalars.first?.properties.isIdeographic ?? false
+                        if isIdeographic {
+                                if !lastWasIdeographic && !otherCache.isEmpty {
+                                        let newElement: (String, Bool) = (otherCache, false)
+                                        blocks.append(newElement)
+                                        otherCache = ""
+                                }
+                                ideographicCache.append(character)
+                                lastWasIdeographic = true
+                        } else {
+                                if lastWasIdeographic && !ideographicCache.isEmpty {
+                                        let newElement: (String, Bool) = (ideographicCache, true)
+                                        blocks.append(newElement)
+                                        ideographicCache = ""
+                                }
+                                otherCache.append(character)
+                                lastWasIdeographic = false
+                        }
+                }
+                if !ideographicCache.isEmpty {
+                        let newElement: (String, Bool) = (ideographicCache, true)
+                        blocks.append(newElement)
+                } else if !otherCache.isEmpty {
+                        let newElement: (String, Bool) = (otherCache, false)
+                        blocks.append(newElement)
+                }
+                return blocks
+        }
+        private func lookup(text: String) -> (text: String, romanizations: [String]) {
+                let filtered: String = filteredCJKV(text: text)
+                let search = Lookup.search(for: filtered)
+                guard filtered != text else { return search }
+                guard !(filtered.isEmpty) else { return search }
+                let transformed = ideographicBlocks(text: text)
+                var handledCount: Int = 0
+                var combinedText: String = ""
+                for item in transformed {
+                        if item.isIdeographic {
+                                let tail = search.text.dropFirst(handledCount)
+                                let suffixCount = tail.count - item.text.count
+                                let selected = tail.dropLast(suffixCount)
+                                combinedText += selected
+                                handledCount += item.text.count
+                        } else {
+                                combinedText += item.text
+                        }
+                }
+                let combinedRomanizations = search.romanizations.map { romanization -> String in
+                        let syllables: [String] = romanization.components(separatedBy: " ")
+                        var index: Int = 0
+                        var newRomanization: String = ""
+                        var lastWasIdeographic: Bool = false
+                        for character in text {
+                                let isIdeographic: Bool = character.unicodeScalars.first?.properties.isIdeographic ?? false
+                                if isIdeographic {
+                                        newRomanization += (syllables[index] + " ")
+                                        index += 1
+                                        lastWasIdeographic = true
+                                } else {
+                                        if lastWasIdeographic {
+                                                newRomanization = String(newRomanization.dropLast())
+                                        }
+                                        newRomanization.append(character)
+                                        lastWasIdeographic = false
+                                }
+                        }
+                        return newRomanization.trimmingCharacters(in: .whitespaces)
+                }
+                return (combinedText, combinedRomanizations)
         }
 }
 
