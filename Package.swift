b134ec18a6e8b413eca68d0e989dc6e917ae56b2
Bing Zheung <bing@ososo.io>
2023-01-06 14:19:27 +0800

    Improve Package configuration

--- Package.swift
+++ Package.swift
@@ -1,5 +1,4 @@
 // swift-tools-version: 5.7
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
@@ -23,13 +22,12 @@ let targetDependencies: [Target.Dependency] = {
 
 let package = Package(
         name: "JyutBot",
+        platforms: [.macOS(.v13)],
         products: [
                 .executable(name: "jyutbot", targets: ["JyutBot"])
         ],
         dependencies: packageDependencies,
         targets: [
-                // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-                // Targets can depend on other targets in this package, and on products in packages this package depends on.
                 .executableTarget(
                         name: "JyutBot",
                         dependencies: targetDependencies
@@ -37,6 +35,6 @@ let package = Package(
                 .testTarget(
                         name: "JyutBotTests",
                         dependencies: ["JyutBot"]
-                ),
+                )
         ]
 )
